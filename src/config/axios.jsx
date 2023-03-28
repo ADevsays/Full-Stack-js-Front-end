3c763c163abc04d93e3cb3b9c8156233f02079f3
RafaelCTobar <rafaelctobar569@gmail.com>
2023-03-27 19:24:05 -0500

    test

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const url = 'https://full-stack-js-proyect-back-end-production.up.railway.app';
+
 const clientAxios = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
+    baseURL: `${url}/api`
 });
 
 export default clientAxios;
